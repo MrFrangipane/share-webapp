8da119b39b233e7372b7371375aee0a1d391011d
MrFrangipane <mister@frangipane.me>
2019-04-07 18:25:54 +0200

    reorganize repo

--- app/upload.php
+++ app/upload.php
@@ -1,11 +1,12 @@
 <?php
 require_once("database.php");
+/*
 $user = $_SERVER['REDIRECT_REMOTE_USER'];
 
 $result = request('SELECT name FROM user_ ORDER BY user_.name DESC');
 print_r($result);
 echo("==============");
-
+*/
 print_r($_REQUEST);
 echo("==============");
 
