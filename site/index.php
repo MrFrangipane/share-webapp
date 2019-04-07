c14a0ababc07743808d0f8d296be3d26c27ef11d
MrFrangipane <mister@frangipane.me>
2019-04-07 18:16:46 +0200

    reorganize repo

--- site/index.php
+++ site/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Frangitron Share</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="site/style.css">
 </head>
 <body>
     <div id="player">
@@ -15,7 +15,7 @@
     <?php include 'app/playlist.php'; ?>
 
     <script src="http://code.jquery.com/jquery-2.2.0.js"></script>
-    <script src="audioplayer.js"></script>
+    <script src="site/audioplayer.js"></script>
     <script>
         setupAudioPlayer();
     </script>
