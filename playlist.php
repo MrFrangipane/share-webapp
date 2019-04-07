217e90243f9914bbdba260dcd71012d34ca68706
MrFrangipane <mister@frangipane.me>
2019-04-07 14:52:15 +0200

    test playlist columns

--- playlist.php
+++ playlist.php
@@ -1,41 +1,14 @@
 <table id="playlist">
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
-    <tr><td><a href="Odezenne-TuPuDuCu.mp3">Odezenne-TuPuDuCu</a></td></tr>
-    <tr><td><a href="T'es triste.mp3">T'es triste</a></td></tr>
-    <tr><td><a href="Whip.mp3">Whip</a></td></tr>
+    <tr>
+        <td><a href="Odezenne-TuPuDuCu.mp3">Odezenne</a></td>
+        <td><a href="Odezenne-TuPuDuCu.mp3">TuPuDuCu</a></td>
+    </tr>
+    <tr>
+        <td><a href="T'es triste.mp3">N'to</a></td>
+        <td><a href="T'es triste.mp3">T'es triste</a></td>
+    </tr>
+    <tr>
+        <td><a href="Whip.mp3">Sonotheque</a></td>
+        <td><a href="Whip.mp3">Whip</a></td>
+    </tr>
 </table>
