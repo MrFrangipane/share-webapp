476454b6516bcb9e9c8659773502382efe6b0e07
MrFrangipane <mister@frangipane.me>
2019-04-07 17:26:32 +0200

    python : try wrap

--- uploader.py
+++ uploader.py
@@ -1,27 +1,32 @@
-import json
-import MySQLdb
+try:
+    import json
+    import MySQLdb
 
 
-class Database(object):
+    class Database(object):
 
-    def __init__(self, config_file):
-        with open(config_file, 'r') as f_config:
-            self.infos = json.load(f_config)
+        def __init__(self, config_file):
+            with open(config_file, 'r') as f_config:
+                self.infos = json.load(f_config)
 
-    def _request(self, request):
-        connexion = MySQLdb.connect(
-            self.infos['host'],
-            self.infos['username'],
-            self.infos['dbname'],
-            self.infos['password']
-        )
-        cursor = connexion.cursor()
-        cursor.execute(request)
-        return cursor
+        def _request(self, request):
+            connexion = MySQLdb.connect(
+                self.infos['host'],
+                self.infos['username'],
+                self.infos['dbname'],
+                self.infos['password']
+            )
+            cursor = connexion.cursor()
+            cursor.execute(request)
+            return cursor
 
-    def authors(self):
-        cursor = self._request("SELECT * from author ORDER BY name DESC")
-        return cursor.fetchall()
+        def authors(self):
+            cursor = self._request("SELECT * from author ORDER BY name DESC")
+            return cursor.fetchall()
 
 
-print(Database("config.json").authors())
+    print(Database("config.json").authors())
+
+except Exception as e:
+    import traceback
+    traceback.print_exc()
