c36e5d6b0a5596aad3737d00e7cbffdbaa43391d
MrFrangipane <mister@frangipane.me>
2019-04-07 16:26:35 +0200

    1st request

--- playlist.php
+++ playlist.php
@@ -4,23 +4,25 @@ require_once 'config.php';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     echo "Connected to $dbname at $host successfully.";
+
+    $request = 'SELECT song.id as id, author.name as author, song.name as song, timestamp FROM song
+                INNER JOIN author ON song.author = author.id
+                ORDER BY author.name, song.timestamp DESC';
+
+    $result = $conn->query($request);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
 ?>
 
-
 <table id="playlist">
-    <tr>
-        <td>Odezene</td><td>TuPuDuCu</td>
-        <td style="display:none;">songs/Odezenne-TuPuDuCu.mp3</td>
-    </tr>
-    <tr>
-        <td>N'to</td><td>T'es triste</td>
-        <td style="display:none;">songs/T'es triste.mp3</td>
-    </tr>
-    <tr>
-        <td>Sonotheque</td><td>Whip</td>
-        <td style="display:none;">songs/Whip.mp3</td>
-    </tr>
+    <?php while ($row = $result->fetch()) : ?>
+        <tr>
+            <td><?php echo htmlspecialchars($row['author']) ?></td>
+            <td><?php echo htmlspecialchars($row['song']) ?></td>
+            <td style="display:none;">songs/<?php echo htmlspecialchars($row['id']) ?>.mp3</td>
+        </tr>
+    <?php endwhile; ?>
 </table>
