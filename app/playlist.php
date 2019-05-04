9db26f1efb60fb4d3d9c4de3cb8ac43c8e2951a4
mrfrangipane <mrfrangipane@frangi.pane>
2019-05-04 11:30:50 +0200

    Author icons

--- app/playlist.php
+++ app/playlist.php
@@ -2,7 +2,7 @@
 require_once("database.php");
 
 $result = request(
-    'SELECT song.id as id, user_.name as author, song.name as song, timestamp FROM song
+    'SELECT song.id as id, user_.name as author, user_.id as user_id, song.name as song, timestamp FROM song
     INNER JOIN user_ ON song.author = user_.id
     ORDER BY user_.name ASC, song.timestamp DESC'
 );
@@ -12,9 +12,10 @@ $result = request(
 <div id="playlistScrollable">
     <table id="playlist">
         <?php while ($row = $result->fetch()) : ?>
-            <tr><td>
-                <p class="title"><?php echo htmlspecialchars($row['song']) ?></p>
-                <p class="details"><?php echo htmlspecialchars($row['author']) ?></p>
+            <tr><td class="authorTd"><img class="authorIcon" src="authors/<?php echo htmlspecialchars($row['user_id']) ?>.png"></td>
+            <td>
+                <p class="songTitle"><?php echo htmlspecialchars($row['song']) ?></p>
+                <p class="songDetails"><?php echo htmlspecialchars($row['author']) ?></p>
                 <p style="display:none;">songs/<?php echo htmlspecialchars($row['id']) ?>.mp3</p>
             </td></tr>
         <?php endwhile; ?>
--- site/index.php
+++ site/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0.7, shrink-to-fit=no">
     <title>Frangitron Share</title>
     <link rel="stylesheet" type="text/css" href="site/style.css">
+    <link rel="icon" type="image/png" href="favicon.png" />
 </head>
 <body>
 
