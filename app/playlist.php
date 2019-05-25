99b603d29b413ed0a9a462bf1de1177d91cb68b9
mrfrangipane <mrfrangipane@frangi.pane>
2019-05-25 08:50:34 +0200

    begin never played icon

--- app/playlist.php
+++ app/playlist.php
@@ -1,23 +1,28 @@
 <?php
 require_once("database.php");
 
-$result = request(
-    'SELECT song.id as id, user_.name as author, user_.id as user_id, song.name as song, timestamp FROM song
+$result = request('
+    SELECT song.id as id, user_.name as author, user_.id as author_id, song.name as song, timestamp FROM song
     INNER JOIN user_ ON song.author = user_.id
-    ORDER BY user_.name ASC, song.timestamp DESC'
-);
+    WHERE song.public = TRUE
+    ORDER BY user_.name ASC, song.timestamp DESC
+');
+
+$played_icon = 'site/icons/never-played.png';
 
 ?>
 
 <div id="playlistScrollable">
     <table id="playlist">
         <?php while ($row = $result->fetch()) : ?>
-            <tr><td class="authorTd"><img class="authorIcon" src="authors/<?php echo htmlspecialchars($row['user_id']) ?>.png"></td>
+            <tr><td class="authorTd"><img class="authorIcon" src="authors/<?php echo htmlspecialchars($row['author']) ?>.png"></td>
             <td>
                 <p class="songTitle"><?php echo htmlspecialchars($row['song']) ?></p>
                 <p class="songDetails"><?php echo htmlspecialchars($row['author']) ?></p>
                 <p style="display:none;">songs/<?php echo htmlspecialchars($row['id']) ?>.mp3</p>
-            </td></tr>
+            </td>
+            <td class="playedTd"><img class="playedIcon" src="<?php echo htmlspecialchars($played_icon) ?>"></td>
+            </tr>
         <?php endwhile; ?>
     </table>
 </div>
